28f9c10f30d0a7cd27fe7bcd97569708fbe681e2
Sergey Nikiforov <04mailbox@gmail.com>
2016-09-25 16:38:43 +0300

    build list view with items filled dummy data

--- app/src/main/java/ua/nikiforov/sunshine/MainActivity.java
+++ app/src/main/java/ua/nikiforov/sunshine/MainActivity.java
@@ -2,6 +2,12 @@ package ua.nikiforov.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> weakForecast = Lists.newArrayList(
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        );
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item_forecast, R.id.list_item_forecast_textview, weakForecast);
+        ListView forecastListView = (ListView)findViewById(R.id.listview_forecast);
+        forecastListView.setAdapter(arrayAdapter);
     }
 }
