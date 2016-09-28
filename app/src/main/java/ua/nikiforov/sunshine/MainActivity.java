41bdeff8fe57aaf228aa5c64a856e128c8428fcf
Sergey Nikiforov <04mailbox@gmail.com>
2016-09-28 22:06:54 +0300

    add refresh command to menu; add INTERNET permissions

--- app/src/main/java/ua/nikiforov/sunshine/MainActivity.java
+++ app/src/main/java/ua/nikiforov/sunshine/MainActivity.java
@@ -2,6 +2,8 @@ package ua.nikiforov.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -29,5 +31,20 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.list_item_forecast, R.id.list_item_forecast_textview, weakForecast);
         ListView forecastListView = (ListView)findViewById(R.id.listview_forecast);
         forecastListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.forecastfragment, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                new FetchWeatherTask().execute();
+        }
+        return true;
     }
 }
